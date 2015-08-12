d5fb4f88746cc6df105c32fd7c5e3bbdec3f4211
Jon Davis <jonerikdavis@gmail.com>
2015-08-11 19:12:51 -0700

    VS 2013 Only

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <Dunjun\Common.hpp>
+
+#include <GLFW\glfw3.h>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello" << std::endl;
+	GLFWwindow* window;
 
-	std::cin.ignore();
+	if (!glfwInit())
+		return EXIT_FAILURE;
 
-	return 0;
+	window = glfwCreateWindow(854, 480, "Dunjun", nullptr, nullptr);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+		
+	glfwMakeContextCurrent(window);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		// Render here
+
+		// Swap back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for events
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	return EXIT_SUCCESS;
 }
